ee3522fcd460ad00fc4b942d9ec0c76b888a855f
Blahut, György <fable3@gmail.com>
2020-05-15 08:00:23 +0200

    smooth random wiggly
    just testing the system
    + vs 2017 project files

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#define _USE_MATH_DEFINES
 #include <uWS/uWS.h>
 #include <fstream>
 #include <iostream>
@@ -98,6 +99,44 @@ int main() {
            *   sequentially every .02 seconds
            */
 
+		  
+		  double pos_x;
+		  double pos_y;
+		  double angle;
+		  int path_size = std::min(3, (int)previous_path_x.size());
+
+		  for (int i = 0; i < path_size; ++i) {
+			  next_x_vals.push_back(previous_path_x[i]);
+			  next_y_vals.push_back(previous_path_y[i]);
+		  }
+
+		  if (path_size == 0) {
+			  pos_x = car_x;
+			  pos_y = car_y;
+			  angle = deg2rad(car_yaw);
+		  }
+		  else {
+			  pos_x = previous_path_x[path_size - 1];
+			  pos_y = previous_path_y[path_size - 1];
+
+			  double pos_x2 = previous_path_x[path_size - 2];
+			  double pos_y2 = previous_path_y[path_size - 2];
+			  angle = atan2(pos_y - pos_y2, pos_x - pos_x2);
+		  }
+
+		  static int counter = 0;
+		  counter++;
+		  double dist_inc = 0.5;
+		  double angle_inc = (pi() / 100)*sin(counter/100.0);
+		  for (int i = 0; i < 50 - path_size; ++i) {
+			  next_x_vals.push_back(pos_x + (dist_inc)*cos(angle + (i + 1)*angle_inc));
+			  next_y_vals.push_back(pos_y + (dist_inc)*sin(angle + (i + 1)*angle_inc));
+			  pos_x += (dist_inc)*cos(angle + (i + 1)*angle_inc);
+			  pos_y += (dist_inc)*sin(angle + (i + 1)*angle_inc);
+		  }
+
+		  
+
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
@@ -125,7 +164,7 @@ int main() {
   });
 
   int port = 4567;
-  if (h.listen(port)) {
+  if (h.listen("127.0.0.1", port)) {
     std::cout << "Listening to port " << port << std::endl;
   } else {
     std::cerr << "Failed to listen to port" << std::endl;
