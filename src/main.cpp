29c335f42a2a5f36e250d121b8079fea4d66413e
Blahut, György <fable3@gmail.com>
2020-05-18 11:07:49 +0200

    offroad mode to get back to road without splines

--- src/main.cpp
+++ src/main.cpp
@@ -316,6 +316,36 @@ struct Map
 		next_waypoint_id = wp_id;
 		return ret;
 	}
+
+	void project_speed(Point speed_vector, int next_wp_id, double *p_vs, double *p_vd)
+	{
+		double rnom, snom, rdenom;
+		Point wp_vector = get_waypoint(next_wp_id).ref - get_waypoint(next_wp_id - 1).ref;
+		
+		double speed_vector_len = speed_vector.length();
+		if (speed_vector_len < EPSILON)
+		{
+			*p_vs = speed_vector_len;
+			*p_vd = 0;
+		}
+		else
+		{
+			double wp_vector_len = wp_vector.length();
+			wp_vector.x *= speed_vector_len / wp_vector_len;
+			wp_vector.y *= speed_vector_len / wp_vector_len;
+			double speed_sign = 1.0;
+
+			if (wp_vector.dotp(speed_vector) < 0)
+			{
+				speed_vector.x *= -1;
+				speed_vector.y *= -1;
+				speed_sign = -1;
+			}
+			distancesq_pt_seg(speed_vector, Point(0, 0), wp_vector, rnom, rdenom, snom);
+			*p_vs = (rnom / rdenom)*speed_vector_len * speed_sign;
+			*p_vd = (snom / rdenom)*speed_vector_len * speed_sign;
+		}
+	}
 };
 
 
@@ -342,7 +372,6 @@ struct SpeedController
 	}
 };
 
-
 struct TrajectoryBuilder
 {
 	double total_dist = 0;
@@ -359,7 +388,7 @@ struct TrajectoryBuilder
 	
 	vector<Point> build(vector<Point> &prev_trajectory,
 		double ego_x, double ego_y, double ego_yaw, // fallback for init if not enough points in prev_trajectory
-		int ego_lane, int target_lane,
+		int ego_lane, int target_lane, double ego_d, double ego_vd,
 		Map &map,
 		SpeedController &speed_controller)
 	{
@@ -394,6 +423,7 @@ struct TrajectoryBuilder
 				angle = atan2(pos_y - pos_y2, pos_x - pos_x2);
 			}
 		}
+		//printf(" ego yaw %.2f angle %.2f", deg2rad(ego_yaw), angle);
 		add_waypoint(Point(pos_x, pos_y)); // startup
 
 		/*
@@ -415,18 +445,98 @@ struct TrajectoryBuilder
 		}*/
 		double min_control_point_dist = speed_controller.start_speed * 1;
 		min_control_point_dist = std::max(min_control_point_dist, 5.0); // at least 5 m
+		//double first_control_point_dist = min_control_point_dist; // first control point is to switch lane or return to center line
 		double contol_point_start_s = 0;
-		if (ego_lane != target_lane)
+		
+		//if (ego_lane != target_lane)
 		{
-			double lane_switch_time = 2.0;
+			// we need more distance if heading the opposite way, and less if car is already aligned
+			// about half the distance if aligned, because we need to steer back to straight
+			double d_diff = map.get_lane_center_offset(target_lane) - ego_d;
+
+			// we have d-wise speed and constant d-wise acceleration
+			// first, we accelerate up, then slow down
+			// a normal lane change is 2 seconds, so we accelerate for 1 seconds, then slow down, and the result distance is 4 meters
+			// first half or second half: 2 = a/2*1*1 => d-wise acceleration is 4 m/s^2
+			// depending on d_diff and ego_vd, we determine if we need to still accelerate or just slow down is enough:
+			// time to stop: t = ego_v/a
+			// d_diff_max = ego_v*t-a/2*t*t and 0=ego_v-a*t
+			// or simply: d_diff_max = ego_v*t/2 = ego_v*ego_v/a/2
+			double d_acc = 4; // d-wise acceleration, m/s^2
+			bool slow_down_phase = false;
+			double lane_switch_time = 2.0; // calculate below
+			if ((ego_vd<0) == (d_diff<0))
+			{
+				double d_diff_max = ego_vd * ego_vd / d_acc / 2;
+				if (d_diff_max > abs(d_diff))
+				{
+					slow_down_phase = true;
+					lane_switch_time = fabs(ego_vd) / d_acc;
+				}
+			}
+			// if we're not slowing down, we accelerate first, then decellerate
+			if (!slow_down_phase)
+			{
+				// we'll reach peak_vd after t1 and decelerate from there back to 0:
+				// decelerate_phase_dist = peak_vd*peak_vd/a/2
+				// accelerate_phase_dist = ego_vd*t+a/2*t*t
+				// acceleration time: t=(peak_vd-ego_vd)/a
+				// accelerate_phase_dist + decelerate_phase_dist = d_diff
+				// so:
+				// ego_vd*t+a/2*t*t + peak_vd*peak_vd/a/2 = d_diff
+				// ego_vd*(peak_vd-ego_vd)/a+a/2*(peak_vd-ego_vd)/a*(peak_vd-ego_vd)/a + peak_vd*peak_vd/a/2  = d_diff
+				// ego_vd*(peak_vd-ego_vd)/a+(peak_vd-ego_vd)*(peak_vd-ego_vd)/a/2 + peak_vd*peak_vd/a/2  = d_diff
+				// 2*ego_vd*(peak_vd-ego_vd)+(peak_vd-ego_vd)*(peak_vd-ego_vd) + peak_vd*peak_vd  = d_diff * a * 2
+				// 2ep-2ee+pp-2pe+ee+pp=d_diff
+				// 2pp-ee = d_diff*a*2
+				// peak_vd = sqrt(d_diff*a+ego_vd*ego_vd/2)
+				// checking with normal change, ego_vd=0, d_diff=4, a=4:
+				// peak_vd = sqrt(4*4) = 4 m/s, average speed is 2 m/s, lane change completes in 2 seconds
+				// checking halfway lane change, ego_vd=4, d_diff=2, a=4:
+				// peak_vd = sqrt(2*4+4*4/2) = 4 m/s lane change completes in 1 second
+
+				// maybe easier another calculation:
+				// accelerate phase from 0: peak_vd*peak_vd/a/2
+				// if ego_vd has the same sign:
+				// accelerate phase from ego_vd: peak_vd*peak_vd/a/2-ego_vd*ego_vd/a/2
+				// total dist: d_diff = peak_vd*peak_vd/a-ego_vd*ego_vd/a/2
+				// peak_vd = sqrt(d_diff*a+ego_vd*ego_vd/2)
+				// need the time for the lane change: t_acc=(peak_vd-ego_vd)/a
+				// t_decelerate = peak_vd/a
+				// total time: (peak_vd*2-ego_vd)/a
+				// if ego_v is negative, we first accelerate back to 0, we'll calculate with relative ego_v, it's relative to d_diff direction
+				double rel_ego_vd = ego_vd;
+				if (d_diff < 0) rel_ego_vd *= -1;
+				double abs_d_diff = fabs(d_diff);
+
+				double peak_vd = sqrt(abs_d_diff * d_acc + rel_ego_vd * rel_ego_vd / 2);
+				lane_switch_time = (peak_vd * 2 - rel_ego_vd) / d_acc;
+				if (lane_switch_time < 0)
+				{
+					// should be handled in slow_down_phase
+					printf("lane_switch_time(%.2f) < 0\n", lane_switch_time);
+				}
+			}
+
+			// we need 0 speed and acceleration when we arrive at the center of target lane
+			// 0 = ego_v + t*acc
+			// d_diff = ego_v*t + acc/2*t^2
+
+			//double vd_diff = fabs(ego_vd); // need to straighten up
+			//double disalignment = fabs(ego_vd - d_diff); // comparing d in 1 sec into future to see disalignment
+			// if disalignment is 0, like middle of lane switch, we need about 1 sec for 2 meter,
+			// if 4 meter, we just start the lane switch, need about 2 seconds
+			// if 6 meter, we might in the middle of an opposite lane change, need about 3 sec
+			//double lane_switch_time = 2.0 * d_diff/4.0+disalignment; // 2 seconds / 4 meter
 			double min_lane_switch_dist = 5.0;
 			double speed = speed_controller.start_speed;
 			double dist = speed * lane_switch_time;
 			if (dist < min_lane_switch_dist) dist = min_lane_switch_dist;
-			double waypoint_dist;
-			int next_wp;
-			add_waypoint(map.get_lane_pos(dist, target_lane, next_wp, waypoint_dist));
+			if (dist > 50) dist = 50; // for offroad
 			contol_point_start_s = dist;
+		}
+		//else
+		{
 		}
 		/*else
 		{
@@ -443,7 +553,7 @@ struct TrajectoryBuilder
 		{
 			double waypoint_dist;
 			int next_wp;
-			contol_point_start_s += min_control_point_dist;
+			
 			Point next_pt = map.get_lane_pos(contol_point_start_s, target_lane, next_wp, waypoint_dist);
 			/*if (waypoint_dist < min_control_point_dist)
 			{
@@ -461,6 +571,7 @@ struct TrajectoryBuilder
 				add_waypoint(next_pt);
 			}
 			if (total_dist > 50 && waypoints.size() > 2) break;
+			contol_point_start_s += min_control_point_dist;
 		}
 
 		if (fLog)
@@ -515,7 +626,7 @@ struct TrajectoryBuilder
 
 		for (int i = 1; i < (int)wp_x.size(); i++)
 		{
-			if (wp_x[i] < wp_x[i - 1])
+			if (wp_x[i] <= wp_x[i - 1])
 			{
 				printf("\nspline input error\n");
 				if (fLog) fprintf(fLog, "spline input error at idx %d x values %.4f<%.4f\n", i, wp_x[i], wp_x[i - 1]);
@@ -524,50 +635,108 @@ struct TrajectoryBuilder
 				break;
 			}
 		}
-		if (wp_x.size() < 3)
-		{
-			return prev_trajectory;
-		}
-		
-		spline.set_points(wp_x, wp_y);
 
 		double dist_total = 0;
 		double current_t = 0.02;
-		//double speed = max_speed;
-		//for (int i = 1; i < (int)waypoints.size(); i++)
-		double current_sp_arg = 0;
-		while (current_sp_arg<50) // 50 meters max, will stop at 50 points anyway
+
+		if (wp_x.size() < 3 || wp_x.size()<prev_trajectory.size() || fabs(ego_d)>20) // offroad is not good with splines
 		{
-			/*if (!in_lane_jmt.empty() && current_t <= in_lane_jmt_max_time)
-			{
-				speed = get_jmt_speed(in_lane_jmt, current_t);
-			}*/
-			// do linear speed, the only important point is the first one, we'll drop the rest in the next cycle,
-			// so can't do smooth stuff
+			// not enough control points, can't generate spline, generate trajectory based on angle
 			double speed = speed_controller.get_speed(current_t);
-			double dist_step = speed / 50;
-			double sp_result = spline(current_sp_arg + dist_step);
-			double x = current_sp_arg + dist_step;
-			double y = sp_result;
-			double dist = distance(pos_x, pos_y, x, y);
-			if (dist+EPSILON < dist_step)
+			double current_angle = 0; // working in transformed space
+			int next_control_point = 1;
+			for (; result_points.size() < 50 && next_control_point<waypoints.size();)
 			{
-				printf("\nspline warning, dist: %.4f, step: %.4f, pos %.2f,%.2f to %.2f,%.2f\n", dist, dist_step, pos_x, pos_y, x, y);
-				if (fLog) fprintf(fLog, "spline warning, dist: %.4f, step: %.4f, pos %.2f,%.2f to %.2f,%.2f\n", dist, dist_step, pos_x, pos_y, x, y);
+				double dist_step = speed / 50;
+				Point next_pt = waypoints[next_control_point];
+				Point current_p = Point(pos_x, pos_y);
+				Point next_delta = (next_pt - current_p);
+				double control_point_dist = next_delta.length();
+				if (control_point_dist < dist_step)
+				{
+					next_control_point++;
+					continue;
+				}
+				current_t += 0.02;
+				//if (speed > EPSILON)
+				{
+					double next_control_point_angle = atan2(next_delta.y, next_delta.x);
+					double angle_diff = fmod(next_control_point_angle - current_angle + 3 * pi(), 2 * pi()) - pi();
+					// v=sqrt(radius)*const, so radius = v^2*K. Also, turn radius is minimum 10 meters.
+					// example values:
+					// 100 m -> 13.4 m/s (K=1/180)
+					// 200 m -> 17.88 m/s (K=1/160)
+					// 300m -> 20 m/s (K=1/133)
+					double min_radius = std::max(10.0, speed*speed / 150);
+					// with this speed, we complete the 2pi circle t= min_radius*2*pi/speed
+					// so in sec/rad: min_radius/speed
+					// we need rad/sec: speed/min_radius
+					double rad_per_sec = speed / min_radius;
+					double max_angle_step =  rad_per_sec / 50;
+					if (fabs(angle_diff) > max_angle_step)
+					{
+						if (angle_diff > 0) current_angle += max_angle_step;
+						else current_angle -= max_angle_step;
+					}
+					else
+					{
+						current_angle += angle_diff;
+					}
+				}
+				//pos_x += next_delta.x*dist_step / control_point_dist;
+				//pos_y += next_delta.y*dist_step / control_point_dist;
+				pos_x += cos(current_angle)*dist_step;
+				pos_y += sin(current_angle)*dist_step;
+				// transform back:
+				Point tp;
+				tp.x = pos_x * cos_angle - pos_y * sin_angle;
+				tp.y = pos_x * sin_angle + pos_y * cos_angle;
+				tp = tp + transform_center;
+				result_points.push_back(tp);
 			}
-			if (current_t == 0.02) printf(" next sp %.2f", speed);
-			current_t += 0.02;
-			double sp_step = (x - pos_x)*dist_step / dist;
-			current_sp_arg += sp_step;
-			pos_x += sp_step;
-			pos_y += (y - pos_y)*dist_step / dist;
-			// transform back:
-			Point tp;
-			tp.x = pos_x * cos_angle - pos_y * sin_angle;
-			tp.y = pos_x * sin_angle + pos_y * cos_angle;
-			tp = tp + transform_center;				
-			result_points.push_back(tp);
-			if (result_points.size() >= 50) break;
+		}
+		else
+		{
+			spline.set_points(wp_x, wp_y);
+
+			//double speed = max_speed;
+			//for (int i = 1; i < (int)waypoints.size(); i++)
+			double current_sp_arg = 0;
+			while (current_sp_arg < 50) // 50 meters max, will stop at 50 points anyway
+			{
+				/*if (!in_lane_jmt.empty() && current_t <= in_lane_jmt_max_time)
+				{
+					speed = get_jmt_speed(in_lane_jmt, current_t);
+				}*/
+				// do linear speed, the only important point is the first one, we'll drop the rest in the next cycle,
+				// so can't do smooth stuff
+				double speed = speed_controller.get_speed(current_t);
+				double dist_step = speed / 50;
+				double sp_result = spline(current_sp_arg + dist_step);
+				double x = current_sp_arg + dist_step;
+				double y = sp_result;
+				double dist = distance(pos_x, pos_y, x, y);
+				if (dist + EPSILON < dist_step)
+				{
+					printf("\nspline warning, dist: %.4f, step: %.4f, pos %.2f,%.2f to %.2f,%.2f\n", dist, dist_step, pos_x, pos_y, x, y);
+					if (fLog) fprintf(fLog, "spline warning, dist: %.4f, step: %.4f, pos %.2f,%.2f to %.2f,%.2f\n", dist, dist_step, pos_x, pos_y, x, y);
+				}
+				if (current_t == 0.02) printf(" next sp %.2f", speed);
+				current_t += 0.02;
+				double sp_step = (x - pos_x)*dist_step / dist;
+				current_sp_arg += sp_step;
+				y = spline(current_sp_arg);
+				pos_x += sp_step;
+				pos_y = y;
+				//pos_y += (y - pos_y)*dist_step / dist;
+				// transform back:
+				Point tp;
+				tp.x = pos_x * cos_angle - pos_y * sin_angle;
+				tp.y = pos_x * sin_angle + pos_y * cos_angle;
+				tp = tp + transform_center;
+				result_points.push_back(tp);
+				if (result_points.size() >= 50) break;
+			}
 		}
 		if (fLog)
 		{
@@ -692,6 +861,7 @@ int main() {
 		  vector<Point> prev_trajectory;
 		  double delta_t0 = 0; // time in the future where trajectory planning starts, at the end of kept previous path
 
+		  Point ego_speed_vector;
 		  int prev_trajectory_length = 4; // need 1 for pos, 2 for speed, 3 for acc, 4 for jerk
 		  if ((int)previous_path_x.size() >= prev_trajectory_length)
 		  {
@@ -702,11 +872,14 @@ int main() {
 			  }
 			  double v1 = (prev_trajectory[1] - prev_trajectory[0]).length();
 			  double v2 = (prev_trajectory[2] - prev_trajectory[1]).length();
-			  double v3 = (prev_trajectory[3] - prev_trajectory[2]).length();
+			  ego_speed_vector = prev_trajectory[3] - prev_trajectory[2];
+			  double v3 = ego_speed_vector.length();
 			  ego_acc = (v3 - v2) * 50;
 			  double prev_acc = (v2 - v1) * 50;
 			  ego_jerk = (ego_acc - prev_acc) * 50;
 			  ego_speed = v3 * 50;
+			  ego_speed_vector.x *= 50;
+			  ego_speed_vector.y *= 50;
 			  ego_x = prev_trajectory[3].x;
 			  ego_y = prev_trajectory[3].y;
 			  delta_t0 = prev_trajectory_length / 50.0;
@@ -736,7 +909,9 @@ int main() {
 			  ego_s = ego_d = 0;
 			  ego_lane = 0;
 		  }
-		  printf("v% 4.2f a% 4.2f d% 4.2f", ego_speed, ego_acc, ego_d);
+		  double ego_vs, ego_vd;
+		  map.project_speed(ego_speed_vector, map.reference_waypoint_id, &ego_vs, &ego_vd);
+		  printf("v% 4.2f a% 4.2f d% 4.2f vd % 4.2f", ego_speed, ego_acc, ego_d, ego_vd);
 		  if (fLog) fprintf(fLog, "v%.2f a%.2f d%.2f\n", ego_speed, ego_acc, ego_d);
 
 		  if (ego_acc > maximum_acc) ego_acc = maximum_acc; // don't try to limit jerk if acc is too high anyway
@@ -763,34 +938,9 @@ int main() {
 			  }
 			  else
 			  {
+				  map.project_speed(Point(car.vx, car.vy), next_wp_id, &car.s, &car.d);
 				  //car.s = car_data[5];
 				  //car.d = car_data[6];
-				  double rnom, snom, rdenom;
-				  Point wp_vector = map.get_waypoint(next_wp_id).ref - map.get_waypoint(next_wp_id-1).ref;
-				  Point speed_vector = Point(car.vx, car.vy);
-				  double speed_vector_len = speed_vector.length();
-				  if (speed_vector_len < EPSILON)
-				  {
-					  car.vs = speed_vector_len;
-					  car.vd = 0;
-				  }
-				  else
-				  {
-					  double wp_vector_len = wp_vector.length();
-					  wp_vector.x *= speed_vector_len / wp_vector_len;
-					  wp_vector.y *= speed_vector_len / wp_vector_len;
-					  double speed_sign = 1.0;
-
-					  if (wp_vector.dotp(speed_vector) < 0)
-					  {
-						  speed_vector.x *= -1;
-						  speed_vector.y *= -1;
-						  speed_sign = -1;
-					  }
-					  distancesq_pt_seg(Point(car.vx, car.vy), Point(0, 0), wp_vector, rnom, rdenom, snom);
-					  car.vs = (rnom / rdenom)*speed_vector_len * speed_sign;
-					  car.vd = (snom / rdenom)*speed_vector_len * speed_sign;
-				  }
 			  }
 		  }
 
@@ -978,7 +1128,7 @@ int main() {
 			  if (ch == 'D' || ch == 'd') target_lane = std::min(2, target_lane + 1);
 			  
 		  }
-		  vector<Point> refined_trajectory = trajectory.build(prev_trajectory, ego_x, ego_y, ego_yaw, ego_lane, target_lane, map, speed_controller);
+		  vector<Point> refined_trajectory = trajectory.build(prev_trajectory, ego_x, ego_y, ego_yaw, ego_lane, target_lane, ego_d, ego_vd, map, speed_controller);
 
 		  vector<double> next_x_vals(refined_trajectory.size());
 		  vector<double> next_y_vals(refined_trajectory.size());
